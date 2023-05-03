6c0cb273ec17f265b9ebcfa3225df1218f52159a
abusayedwd <110084857+abusayedwd@users.noreply.github.com>
2023-05-03 17:13:02 +0600

    gitignore file create

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
 const express = require('express')
 const app = express();
 const port = process.env.PORT || 5000;
+const cors = require('cors')
 
 
+
+const recipes = require('./data/recipe.json')
+const chefs = require('./data/chefs.json')
+
+app.use(cors())
 app.get('/', (req,res) => {
         res.send('food is running')
 })
+
+app.get('/chefs', (req,res) => {
+        res.send(chefs)
+} )
+
+
+
+app.get('/chef/:id', (req,res)=> {
+        const id = req.params.id;
+  const singleChef = chefs.find(chef => chef.id ==id) || [];
+  res.send(singleChef);
+})
+app.get('/recipes', (req,res) => {
+   res.send(recipes)
+})
+
+
 app.listen(port, () => {
         console.log(`the food is running now ${port}`)
 })
