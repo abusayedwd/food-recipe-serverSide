cbadef7050450821e14344887dc83691c752af55
abusayedwd <110084857+abusayedwd@users.noreply.github.com>
2023-05-04 00:31:00 +0600

    added to recipe json

--- index.js
+++ index.js
@@ -2,14 +2,10 @@ const express = require('express')
 const app = express();
 const port = process.env.PORT || 5000;
 const cors = require('cors')
-
-
-
+ 
 const recipes = require('./data/recipe.json')
 const chefs = require('./data/chefs.json')
-
-
-
+ 
 app.use(cors())
 app.get('/', (req,res) => {
         res.send('food is running')
@@ -19,16 +15,16 @@ app.get('/chefs', (req,res) => {
         res.send(chefs)
 } )
 
-
+app.get('/recipes', (req,res) => {
+        res.send(recipes)
+     })
 
 app.get('/chef/:id', (req,res)=> {
         const id = req.params.id;
   const singleChef = chefs.find(chef => chef.id ==id) || [];
   res.send(singleChef);
 })
-app.get('/recipes', (req,res) => {
-   res.send(recipes)
-})
+
 
 
 app.listen(port, () => {
